2b4c38677ade5980883d1ce5056b9e1b8e21688d
Kruchyxe <krzysiekmilczanowski@interia.eu>
2022-02-11 13:02:41 +0100

    Initial commit

---
+++ src/main/java/pl/kruchyxe/hellhealth/HellHealthApplication.java
@@ -0,0 +1,13 @@
+package pl.kruchyxe.hellhealth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellHealthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HellHealthApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/kruchyxe/hellhealth/HellHealthApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kruchyxe.hellhealth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellHealthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
